694179b4fa6e325e348a27b147ce06481612b249
gilbmura <gilbertmuramira@gmail.com>
2025-02-14 11:38:37 -0800

    Changed Days Reamaining

--- create_environment.sh
+++ create_environment.sh
@@ -27,7 +27,7 @@ submissions_file="../assets/submissions.txt"
 
 # Print remaining time and run the reminder function
 echo "Assignment: $ASSIGNMENT"
-echo "Days remaining to submit: $TIME_REMAINING days"
+echo "Days remaining to submit: $DAYS_REMAINING days"
 echo "--------------------------------------------"
 
 check_submissions $submissions_file
