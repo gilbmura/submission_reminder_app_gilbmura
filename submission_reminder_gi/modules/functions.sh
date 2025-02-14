d0077f508bbe977a5752f6f085c01d43e58863ba
gilbmura <gilbertmuramira@gmail.com>
2025-02-14 05:02:37 -0800

    testing changes

--- submission_reminder_gi/modules/functions.sh
+++ submission_reminder_gi/modules/functions.sh
@@ -4,7 +4,6 @@ function check_submissions() {
     local submissions_file=$1
     echo "Checking submissions in $submissions_file"
 
-    # Read the submissions file and process it line by line
     while IFS=, read -r student assignment status; do
         if [[ "$assignment" == "$ASSIGNMENT" && "$status" == " not submitted" ]]; then
             echo "Reminder: $student has not submitted the $ASSIGNMENT assignment!"
